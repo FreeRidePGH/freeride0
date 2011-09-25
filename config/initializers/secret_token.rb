e751670ab459367c75ddd802cbe899272a37ab9d
Tyler Fox <tmfox@andrew.cmu.edu>
2011-09-25 15:18:11 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Freeride::Application.config.secret_token = '6fd563917887753002c6087c47551db7c50938eeafb5a3f1dbee82b24aedbd75fb7a996978a48f7b29ce46a5f25e64961c58f6d1348942ea6117105b640f3174'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Freeride::Application.config.session_store :cookie_store, key: '_freeride_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Freeride::Application.config.session_store :active_record_store
