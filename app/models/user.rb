8349131f892210fc754d866fa7e04931a5b16e23
Tyler Fox <tmfox@andrew.cmu.edu>
2011-11-29 16:30:27 -0500

    reformatted tables on dashboard

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 require 'digest'
 
 class User < ActiveRecord::Base
-  has_many :eab_projects
+  has_many :eab_projects, :order => "start_date DESC"
   has_many :repair_hours_entries
   has_many :volunteer_hours_entries
   has_many :transactions, :order => "created_at DESC"
-  has_many :recent_transactions, :class_name => "Transaction", :order => "created_at DESC", :limit => 3
-  has_many :favorites
+  has_many :recent_transactions, :class_name => "Transaction", :order => "created_at DESC", :limit => 5
+  has_many :favorites, :order => "created_at DESC"
   
   attr_accessor :password # using attr_accessor :password creates a virtual password attribute, not stored in database
   attr_accessor :phone1, :phone2, :phone3
