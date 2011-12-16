0c9c8a102c5884ef801c409e416e147eccb948cc
Tyler Fox <tmfox@andrew.cmu.edu>
2011-12-16 13:31:03 -0800

    changed cap deploy file

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 set :application, "freeride"
-set :repository,  "git@freeride.andrew.cmu.edu:/home/git/freeride.git"
+set :repository,  "git@localhost:/home/git/freeride.git"
 
 set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
@@ -7,7 +7,7 @@ set :scm, :git
 role :web, "freeride.andrew.cmu.edu"                          # Your HTTP server, Apache/etc
 role :app, "freeride.andrew.cmu.edu"                          # This may be the same as your `Web` server
 role :db,  "freeride.andrew.cmu.edu", :primary => true # This is where Rails migrations will run
-role :db,  "freeride.andrew.cmu.edu"
+# role :db,  "freeride.andrew.cmu.edu"
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
