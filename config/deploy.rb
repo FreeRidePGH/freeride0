8181d1b421e8bffafc84b08cff0cdd283d2f2198
Tyler Fox <tmfox@andrew.cmu.edu>
2011-12-16 13:36:30 -0800

    changed deploy file

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,6 +9,8 @@ role :app, "freeride.andrew.cmu.edu"                          # This may be the 
 role :db,  "freeride.andrew.cmu.edu", :primary => true # This is where Rails migrations will run
 # role :db,  "freeride.andrew.cmu.edu"
 
+default_run_options[:pty] = true
+
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
 
